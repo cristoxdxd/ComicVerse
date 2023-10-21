dad929273530aca76be897165149a8851396ece7
Cristopher Herrera <cristopher.herrera@epn.edu.ec>
2023-10-20 19:59:36 -0500

    fix: build error

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { NotFound } from './Pages/404/NotFound.tsx'
